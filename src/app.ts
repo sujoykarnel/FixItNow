2be79cdb26ad5667f3272c313f4cd3b2b7ffd71d
Sujoy Das <sujoykarnel@gmail.com>
2026-07-09 16:01:32 +0600

    not found & global error handler done

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,38 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors'
-import config from './config';
-import cookieParser from 'cookie-parser';
-import { authRouter } from './modules/Auth/auth.route';
-import { categoryRouter } from './modules/category/category.route';
-import { serviceRouter } from './modules/service/service.route';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
+import { authRouter } from "./modules/Auth/auth.route";
+import { categoryRouter } from "./modules/category/category.route";
+import { serviceRouter } from "./modules/service/service.route";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
 
 const app: Application = express();
 
 app.use(
-    cors({
-        origin: config.app_url,
-        credentials: true
-    })
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello, FixItNow")
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, FixItNow");
 });
 
-
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/api/services", serviceRouter);
+
+// not found handler
+app.use(notFound);
+
+// global error handler
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
+import httpStatus from "http-status";
+
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode;
+  let errorMessage = err.message || "Internal server error";
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = httpStatus.BAD_REQUEST;
+    errorMessage =
+      "You have to provided incorrect field type of missing fields";
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Duplicate key error";
+    } else if (err.code === "P2003") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Foreign key constraint failed";
+    } else if (err.code === "P2025") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage =
+        "An operational failed because it depends on one or more records tha were required but not found";
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = httpStatus.UNAUTHORIZED;
+      errorMessage =
+        "Authentication failed against database server at {database_host}, the provided database credentials";
+    } else if (err.errorCode === "P1001") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Can't reach database server";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    errorMessage = "Error occurred during query execution";
+  }
+
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+    name: err.name || "Internal server error",
+    message: errorMessage,
+    error: err.stack,
+  });
+};
